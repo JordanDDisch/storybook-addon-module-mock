238b62b3016d2953f27da720abac2246f6f2b71f
Jordan Disch <jordandisch@Jordans-MacBook-Pro.local>
2024-01-10 16:22:07 +1100

    downgraded minimatch

--- src/plugins/babel-import-writer.ts
+++ src/plugins/babel-import-writer.ts
@@ -1,5 +1,5 @@
 import { types as t, PluginObj, template, NodePath } from '@babel/core';
-import { minimatch } from 'minimatch';
+import minimatch from 'minimatch';
 import { AddonOptions } from '../types';
 
 const buildMocks = template(`
